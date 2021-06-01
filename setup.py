ac679c24d677f9a402dec836bf66d48412e3e0b1
Niko Abeler <niko@rerere.org>
2021-06-01 22:15:53 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="wahlprogramme",
-    version="1.0.0",
+    version="1.1.0",
     packages=["wahlprogramme"],
     install_requires=["Flask>=2.0.0", "seaborn>=0.11.1", "PyYAML==5.4.1"],
     extras_require={"test": ["pytest==6.2.4"]},
